827016f93f57a5d4a8a0da4c624dbb317c7d6278
Vito290500 <divenosavito00@gmail.com>
2024-01-11 14:43:25 +0100

    Added the pagination and the table of 'archivio pazienti section'

--- section_medico/views.py
+++ section_medico/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.views import View
 
@@ -15,4 +16,27 @@ def NuovoAssistito(request):
 
 
 def ArchivioPazienti(request):
-    return render(request, "structure/includes/archiviopazienti.html")
+    name_example = [
+    'Achille', 'Achille', 'Ada', 'Adam', 'Adelaide', 'Adele', 'Aden', 'Adolfo', 'Adriana', 'Adriano',
+    'Agape', 'Agata', 'Agatha', 'Agnese', 'Agostina', 'Agostino', 'Aiace', 'Aida', 'Akira', 'Alba',
+    'Albarosa', 'Alberico', 'Albert', 'Alberta', 'Alberto', 'Aldo', 'Alejandro', 'Alessandra', 'Alessandro',
+    'Alessia', 'Alessio', 'Alexander', 'Alfio', 'Alfonso', 'Alfredo', 'Alice',
+]
+
+    data = []
+
+    for name in name_example:
+        data.append({
+            'cognome': name,
+            'nome': '-',
+            'Data di nascita': '-',
+            'Codice Fiscale': '-',
+            'Indirizzo': '-',
+            'Dettagli': '-',
+            'Fine attività': '-'
+        })
+
+    print(data)
+
+    data_json = json.dumps(data)
+    return render(request, "structure/includes/archiviopazienti.html", {'data_json': data_json})
