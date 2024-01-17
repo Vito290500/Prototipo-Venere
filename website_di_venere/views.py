d1045306987bc0405b9a9e6a75dd99fac73c4138
Vito290500 <divenosavito00@gmail.com>
2024-01-17 12:03:28 +0100

    deploy on github

--- website_di_venere/views.py
+++ website_di_venere/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "structure/website_starting_page.html")
+    return render(request, "structure/index.html")
 
