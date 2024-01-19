d5101077f8b82466ed9e2af9b9a6ca43145b938b
Vito290500 <divenosavito00@gmail.com>
2024-01-19 11:36:58 +0100

    fix 'nuovo assistito' media query and implemented the 'elenco regioni'

--- section_medico/views.py
+++ section_medico/views.py
@@ -10,7 +10,34 @@ class LoginMedico(View):
         return render(request, "structure/HomePageMedico.html")
 
 def NuovoAssistito(request):
-    return render(request, "structure/includes/nuovoassistito.html")
+
+    elenco_provincie = [
+        'Agrigento, AG', 'Alessandria, AL', 'Ancona, AN', 'Aosta/Aoste, AO'
+        'Arezzo, AR' , 'Ascoli Piceno, AP',	 'Asti,	AT', 'Avellino,	AV', 'Bari,	BA',
+        'Barletta-Andria-Trani,BT', 'Belluno, BL','Benevento, BN',  'Bergamo,BG',
+        'Biella,BI	' ,'Bologna,BO'	 ,'Bolzano,BZ','Brescia,BS'	,'Brindisi,BR','Cagliari,CA',
+        'Caltanissetta,CL' ,'Campobasso,CB','Caserta,CE', 'Catania,CT', "Catanzaro,CZ",
+        "Chieti,CH","Como,CO","Cosenza,CS","Cremona,CR","Crotone,KR","Cuneo,CN","Enna,EN",
+        "Fermo,FM","Ferrara,FE","Firenze,FI","Foggia,FG","Forlì-Cesena,FC","Frosinone,FR",
+        "Genova,GE","Gorizia,GO","Grosseto,GR","Imperia,IM","Isernia,IS","L'Aquila,AQ",
+        "La Spezia,SP","Latina,LT","Lecce,LE","Lecco,LC","Livorno,LI","Lodi,LO","Lucca,LU",
+        "Macerata,MC","Mantova,MN","Massa-Carrara,MS","Matera,MT","Messina,ME","Milano,MI",
+        "Modena,MO","Monza e Brianza, MB", "Novara, NO", "Nuoro, NU", "Oristano, OR", "Padova, PD",
+        "Palermo, PA", "Parma, PR", "Pavia, PV", "Perugia, PG", "Pesaro e Urbino, PU",
+        "Pescara, PE", "Piacenza, PC", "Pisa, PI", "Pistoia, PT", "Pordenone, PN",
+        "Potenza, PZ", "Prato, PO", "Ragusa, RG", "Ravenna, RA", "Reggio Calabria, RC",
+        "Reggio Emilia, RE", "Rieti, RI", "Rimini, RN", "Roma, RM", "Rovigo, RO",
+        "Salerno, SA", "Sassari,SS", "Savona,SV", "Siena,SI", "Siracusa,SR", "Sondrio,SO",
+        "Sud Sardegna,SU", "Taranto,TA", "Teramo,TE", "Terni,TR", "Torino,TO",
+        "Trapani,TP", "Trento,TN", "Treviso,TV", "Trieste,TS", "Udine,UD",
+        "Varese,VA", "Venezia,VE", "Verbano-Cusio-Ossola,VB", "Vercelli,VC",
+        "Verona,VR", "Vibo Valentia,VV", "Vicenza,VI", "Viterbo,VT"
+    ]
+    return render(request, "structure/includes/nuovoassistito.html", {
+        "province" : elenco_provincie
+    })
+
+
 
 def ArchivioPazienti(request):
     name_exam = ['Breath Test Helicobacter Pylori', 'Breath Test Lattosio', 'Breath Test Lattosio',
@@ -22,7 +49,7 @@ def ArchivioPazienti(request):
     'Achille', 'Achille', 'Ada', 'Adam', 'Adelaide', 'Adele', 'Aden', 'Adolfo', 'Adriana', 'Adriano',
     'Agape', 'Agata', 'Agatha', 'Agnese', 'Agostina', 'Agostino', 'Aiace', 'Aida', 'Akira', 'Alba',
     'Albarosa', 'Alberico', 'Albert', 'Alberta', 'Alberto', 'Aldo', 'Alejandro', 'Alessandra', 'Alessandro',
-    'Alessia', 'Alessio', 'Alexander', 'Alfio', 'Alfonso', 'Alfredo', 'Alice',
+    'Alessia', 'Alessio', 'Alexander', 'Alfio', 'Alfonso', 'Alfredo', 'Alice', 'Martina'
     ]
 
     data = []
